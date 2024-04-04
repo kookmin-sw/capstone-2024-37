9aa9b29c4970da2f211aa97223fc9df9733e421f
chrisais9 <chrisais9@naver.com>
2024-04-05 02:30:05 +0900

    (add) landing page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
-const inter = Inter({ subsets: ["latin"] });
+const notoSans = Noto_Sans_KR({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={notoSans.className}>
+        <div className="fixed w-screen flex h-[80px] px-12 items-center justify-between backdrop-blur-sm z-[99]">
+          <div className="flex items-center justify-center gap-4">
+            <Image src="/icons/logo.png" width={48} height={48} alt="" />
+            <div className="bold text-3xl">췍봇</div>
+          </div>
+          <div className="flex justify-center items-center gap-2">
+            <Button variant="secondary">로그인</Button>
+            <Button>회원가입</Button>
+          </div>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
