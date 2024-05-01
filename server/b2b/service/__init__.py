d6a253f733675542150cd06e9bb0b25aa5a67cbd
dawit0905 <dawit0905@gmail.com>
2024-05-01 23:05:26 +0900

    (fix) jwt error

--- server/b2b/service/__init__.py
+++ server/b2b/service/__init__.py
@@ -3,5 +3,6 @@ from service.auth_google_service import sign_up
 from service.auth_google_service import insert_userinfo
 
 from service.user_service import get_user_in_token
+from service.user_service import get_user_in_db
 
 from service.chroma_db_service import *
--- server/b2b/service/user_service.py
+++ server/b2b/service/user_service.py
@@ -3,7 +3,7 @@ import util
 from database import User
 
 
-async def get_user_in_db(google_email: str) -> User:
+async def get_user_in_db(google_email: str) -> dict:
     current_user = await database.repo.find_user_by_google_email(google_email)
     return current_user
 
