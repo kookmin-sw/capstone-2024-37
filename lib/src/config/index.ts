a38a40bd10c0990b2bb7ebeb717ffcb7ac4a53f1
chrisais9 <chrisais9@naver.com>
2024-05-06 14:41:35 +0900

    (fix) tsup config

---
+++ lib/src/config/index.ts
@@ -0,0 +1,1 @@
+export const API_SERVER = "http://43.203.217.112:9000";
--- lib/tsup.config.ts
+++ lib/tsup.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  // The file we created above that will be the entrypoint to the library.
-  entry: ["src/index.tsx"],
-  // Enable TypeScript type definitions to be generated in the output.
-  // This provides type-definitions to consumers.
-  dts: true,
-  // Clean the `dist` directory before building.
-  // This is useful to ensure the output is only the latest.
+  entry: ["src/index.ts"],
+  treeshake: true,
+  sourcemap: "inline",
+  minify: true,
   clean: true,
-  // Sourcemaps for easier debugging.
-  sourcemap: true,
+  dts: true,
+  splitting: false,
+  format: ["cjs", "esm"],
+  external: ["react"],
+  injectStyle: false,
 });
