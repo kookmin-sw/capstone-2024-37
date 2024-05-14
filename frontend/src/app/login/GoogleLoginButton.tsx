e72f98d88001683ad12edf24425e6cc9c17bd0a3
Godong <godong9985@gmail.com>
2024-05-14 18:59:41 +0900

    (add) google login

--- frontend/src/app/login/GoogleLoginButton.tsx
+++ frontend/src/app/login/GoogleLoginButton.tsx
@@ -1,12 +1,73 @@
 "use client";
 
+import { userAtom } from "@/atoms/userAtom";
+import { API_SERVER } from "@/config";
 import { useGoogleLogin } from "@react-oauth/google";
+import { useAtom } from "jotai";
 import Image from "next/image";
 
 const GoogleLoginButton = () => {
+  const [user, setUser] = useAtom(userAtom);
+
   const login = useGoogleLogin({
-    onSuccess: (tokenResponse) => console.log(tokenResponse),
+    onSuccess: async (tokenResponse) => {
+      console.log(tokenResponse);
+      const response = await sendToken(tokenResponse.access_token);
+      if (response) {
+        await getUserInfo(response);
+      }
+    },
+    onError: (error) => {
+      console.error("Login Failed:", error);
+      alert("로그인 실패");
+    },
   });
+
+  async function sendToken(tokenRes) {
+    try {
+      const res = await fetch(`${API_SERVER}/auth/sign-in`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          type: "google",
+          token: tokenRes,
+        }),
+      });
+      const response = await res.json();
+      console.log(response);
+      return response;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async function getUserInfo(userToken) {
+    try {
+      const res = await fetch(`${API_SERVER}/user/me`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+          "Content-type": "application/json",
+        },
+      });
+      const userInfo = await res.json();
+      setUser({
+        token: userToken,
+        email: userInfo.email,
+        clientID: userInfo.client_id,
+        company: userInfo.company_name,
+      });
+      window.location.href = "/";
+      console.log("토큰", userToken);
+      console.log("로그인 성공");
+    } catch (e) {
+      console.error("로그인 실패");
+      alert("로그인 실패");
+    }
+  }
 
   return (
     <button onClick={() => login()}>
