459608c5408b781725d85ff4c8dce84fe0ddf15c
chrisais9 <chrisais9@naver.com>
2024-05-22 01:00:21 +0900

    (fix) version

--- lib/src/ChatBot/ChatView.tsx
+++ lib/src/ChatBot/ChatView.tsx
@@ -34,7 +34,7 @@ const ChatView: React.FC<ChatViewProps> = ({ clientId }) => {
       question: trimmedQuestion,
     });
     try {
-      const res = await fetch(`${API_SERVER}/chat/chatbot-chain`, {
+      const res = await fetch(`${API_SERVER}/chat/chatbot`, {
         method: "POST",
         headers: {
           Accept: "application/json",
